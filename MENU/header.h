7829540ef56df5c08b83aac2c294c321916f1822
Me710 <christianparfait.nebotfonkou@esprit.tn>
2021-04-12 22:07:52 +0100

    MENU AVEC BACKGROUNDQUITTER

---
+++ MENU/header.h
@@ -0,0 +1,20 @@
+#ifndef HEADER_H_INCLUDED
+#define HEADER_H_INCLUDED
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <SDL/SDL.h>
+#include <stdbool.h>
+#include <math.h>
+#include <SDL/SDL_image.h>
+#include <SDL/SDL_ttf.h>
+#include <SDL/SDL_mixer.h>
+#include "intro.h"
+#include "fonction.h"
+#include "option.h"
+#include "gfxutils.h"
+#include "quitter.h"
+#include "stdinc.h"
+#include "defs.h"
+
+#endif
